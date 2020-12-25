fd1537630da6cc01f55d16a5b8ee794420721e86
Humaid Khan <humaidk2@gmail.com>
2020-12-25 15:31:18 +0400

    Create sequelize models for user and diary entries

---
+++ src/models/entry.ts
@@ -0,0 +1,21 @@
+import { Sequelize, Model, DataTypes, ModelCtor } from 'sequelize'
+
+interface EntryInstance extends Model {
+    Id: string
+    data: string
+    createdAt: Date
+    updatedAt: Date
+}
+
+export default function (sequelize: Sequelize): ModelCtor<EntryInstance> {
+    return sequelize.define<EntryInstance>('Entry', {
+        Id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+        data: {
+            type: DataTypes.STRING,
+        },
+    })
+}
