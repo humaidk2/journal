485cca745f50adc79e21af330ad9d1ed00c0ed4b
Humaid Khan <humaidk2@gmail.com>
2020-12-26 16:35:42 +0400

    Integrate bodyparser for to get access to post form data

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,36 @@
 import express = require('express')
 import helmet = require('helmet')
-import userRouter from './routes/user'
-import entryRouter from './routes/entry'
+import bodyParser = require('body-parser')
+import userRouterCreator from './routes/user'
+import entryRouterCreator from './routes/entry'
+import createDb from './models/'
 
+const db = createDb(process.env.JOURNAL_DB_NAME)
+const { User, Entry, sequelize } = db
+sequelize.sync({ force: true })
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
+const userRouter = userRouterCreator(User)
+const entryRouter = entryRouterCreator(Entry, User)
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 app.use(helmet())
 app.set('json spaces', 2)
+
 app.use('/user', userRouter)
 app.use('/entry', entryRouter)
+
+app.get('/createdb', async (req: any, res: any) => {
+    await sequelize.sync({ force: true })
+    await res.send({})
+})
 app.use('/*', (req, res) => {
     res.status(404).send({})
 })
