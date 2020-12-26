3ec0a08a6f408ac5c4d869eb5b57dd3f9c12c147
Humaid Khan <humaidk2@gmail.com>
2020-12-26 16:36:41 +0400

    Integrate sequelize to entry routes and add dependencies for user testing to come first

--- src/routes/entry.test.ts
+++ src/routes/entry.test.ts
@@ -2,6 +2,8 @@ import chai = require('chai')
 import chaiHttp = require('chai-http')
 import server from '../server'
 
+require('./user.test')
+
 const expect = chai.expect
 
 chai.use(chaiHttp)
@@ -10,34 +12,60 @@ const entries = [
     {
         Id: 'entry0',
         data: 'I live the best life',
-        createdAt: '05-12-2020',
-        updatedAt: '05-12-2020',
         UserId: 'user0',
     },
     {
         Id: 'entry1',
         data: 'I live the coolest life',
-        createdAt: '08-12-2020',
-        updatedAt: '08-12-2020',
         UserId: 'user0',
     },
     {
         Id: 'entry2',
         data: 'I live the weirdest life',
-        createdAt: '10-12-2020',
-        updatedAt: '10-12-2020',
         UserId: 'user0',
     },
 ]
 
 describe('Entry route', () => {
+    describe('/POST entry', () => {
+        it('it should insert an Entry', (done) => {
+            chai.request(server)
+                .post('/entry')
+                .type('form')
+                .send(entries[0])
+                .end((err, res) => {
+                    expect(res.status).to.equal(200)
+                    expect(res.body.Id).to.equal(entries[0].Id)
+                    expect(res.body.data).to.equal(entries[0].data)
+                    expect(res.body.UserId).to.equal(entries[0].UserId)
+                    done()
+                })
+        })
+    })
     describe('/GET entry', () => {
+        before(async () => {
+            const requests = []
+            for (let i = 1; i < entries.length; i++) {
+                requests.push(
+                    await chai
+                        .request(server)
+                        .post('/entry')
+                        .type('form')
+                        .send(entries[i])
+                )
+            }
+            await Promise.all(requests)
+        })
         it('it should GET all entries', (done) => {
             chai.request(server)
                 .get('/entry')
                 .end((err, res) => {
                     expect(res.status).to.equal(200)
-                    expect(res.body).to.deep.equal(entries)
+                    for (let i = 0; i < entries.length; i++) {
+                        expect(res.body[i].Id).to.equal(entries[i].Id)
+                        expect(res.body[i].data).to.equal(entries[i].data)
+                        expect(res.body[i].UserId).to.equal(entries[i].UserId)
+                    }
                     done()
                 })
         })
@@ -46,7 +74,11 @@ describe('Entry route', () => {
                 .get('/entry/user0')
                 .end((err, res) => {
                     expect(res.status).to.equal(200)
-                    expect(res.body).to.deep.equal(entries)
+                    for (let i = 0; i < entries.length; i++) {
+                        expect(res.body[i].Id).to.equal(entries[i].Id)
+                        expect(res.body[i].data).to.equal(entries[i].data)
+                        expect(res.body[i].UserId).to.equal(entries[i].UserId)
+                    }
                     done()
                 })
         })
@@ -55,7 +87,9 @@ describe('Entry route', () => {
                 .get('/entry/user0/entry0')
                 .end((err, res) => {
                     expect(res.status).to.equal(200)
-                    expect(res.body).to.deep.equal(entries[0])
+                    expect(res.body.Id).to.equal(entries[0].Id)
+                    expect(res.body.data).to.equal(entries[0].data)
+                    expect(res.body.UserId).to.equal(entries[0].UserId)
                     done()
                 })
         })
--- src/routes/entry.ts
+++ src/routes/entry.ts
@@ -1,50 +1,68 @@
 import express = require('express')
 import { Request, Response } from 'express'
-const router = express.Router({ mergeParams: true })
 
-const entries = [
-    {
-        Id: 'entry0',
-        data: 'I live the best life',
-        createdAt: '05-12-2020',
-        updatedAt: '05-12-2020',
-        UserId: 'user0',
-    },
-    {
-        Id: 'entry1',
-        data: 'I live the coolest life',
-        createdAt: '08-12-2020',
-        updatedAt: '08-12-2020',
-        UserId: 'user0',
-    },
-    {
-        Id: 'entry2',
-        data: 'I live the weirdest life',
-        createdAt: '10-12-2020',
-        updatedAt: '10-12-2020',
-        UserId: 'user0',
-    },
-]
+export default function (Entry: any, User: any) {
+    const router = express.Router({ mergeParams: true })
 
-router.get('/', async (req: Request, res: Response) => {
-    res.status(200).send(entries)
-})
-router.get('/:userId', async (req: Request, res: Response) => {
-    const filteredEntries = entries.filter(
-        (entry) => entry.UserId === req.params.userId
-    )
-    if (filteredEntries.length !== 0) res.status(200).send(filteredEntries)
-    else res.status(404).send({ message: 'User has no entries' })
-})
+    const entries = [
+        {
+            Id: 'entry0',
+            data: 'I live the best life',
+            createdAt: '05-12-2020',
+            updatedAt: '05-12-2020',
+            UserId: 'user0',
+        },
+        {
+            Id: 'entry1',
+            data: 'I live the coolest life',
+            createdAt: '08-12-2020',
+            updatedAt: '08-12-2020',
+            UserId: 'user0',
+        },
+        {
+            Id: 'entry2',
+            data: 'I live the weirdest life',
+            createdAt: '10-12-2020',
+            updatedAt: '10-12-2020',
+            UserId: 'user0',
+        },
+    ]
+    router.post('/', async (req: Request, res: Response) => {
+        try {
+            const entry = await Entry.create({
+                ...req.body,
+            })
+            if (entry) res.status(200).send(entry)
+            else res.status(404).send({ message: 'Entry not inserted' })
+        } catch (error) {
+            await res.status(404).send({ error })
+        }
+    })
+    router.get('/', async (req: Request, res: Response) => {
+        try {
+            const entries = await Entry.findAll()
+            if (entries) res.status(200).send(entries)
+            else res.status(404).send({})
+        } catch (error) {
+            await res.status(404).send({ error })
+        }
+    })
+    router.get('/:userId', async (req: Request, res: Response) => {
+        const filteredEntries = entries.filter(
+            (entry) => entry.UserId === req.params.userId
+        )
+        if (filteredEntries.length !== 0) res.status(200).send(filteredEntries)
+        else res.status(404).send({ message: 'User has no entries' })
+    })
 
-router.get('/:userId/:entryId', async (req: Request, res: Response) => {
-    const entry = entries.find(
-        (entry) =>
-            entry.UserId === req.params.userId &&
-            entry.Id === req.params.entryId
-    )
-    if (entry) res.status(200).send(entry)
-    else res.status(404).send({ message: 'Entry not found' })
-})
-
-export default router
+    router.get('/:userId/:entryId', async (req: Request, res: Response) => {
+        const entry = entries.find(
+            (entry) =>
+                entry.UserId === req.params.userId &&
+                entry.Id === req.params.entryId
+        )
+        if (entry) res.status(200).send(entry)
+        else res.status(404).send({ message: 'Entry not found' })
+    })
+    return router
+}
