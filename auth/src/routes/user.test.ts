0eefe043d67896cbbd6f107a70e5994970e94489
Humaid Khan <humaidk2@gmail.com>
2020-12-28 14:32:57 +0400

    Add tests for all auth routes: signup,login,verifyEmail,refresh,update

--- auth/src/routes/user.test.ts
+++ auth/src/routes/user.test.ts
@@ -6,59 +6,107 @@ const expect = chai.expect
 
 chai.use(chaiHttp)
 
-const users = [
+const users: any = [
     {
         Id: 'user0',
         username: 'humaidk2',
+        email: 'hum@google.com',
         password: 'password',
         firstName: 'humaid',
         lastName: 'khan',
-        dateOfBirth: '2020-04-13T00:00:00.000+08:00',
+        dateOfBirth: new Date('2020-04-13'),
+        encryptedKey: 'key0123',
     },
 ]
 
 describe('User route', () => {
-    describe('/POST user', () => {
-        it('it should insert a User', (done) => {
+    describe('Register user', () => {
+        let emailToken = ''
+        it('it should signup a new User', (done) => {
             chai.request(server)
-                .post('/user')
+                .post('/user/signup')
                 .type('form')
                 .send(users[0])
                 .end((err, res) => {
                     expect(res.status).to.equal(200)
-                    expect(res.body.Id).to.equal(users[0].Id)
+                    expect(res.body.token).to.be.a('string')
+                    emailToken = res.body.token
+                    done()
+                })
+        })
+        it('it should verify a new User', (done) => {
+            chai.request(server)
+                .get(`/user/verifyEmail?token=${emailToken}`)
+                .type('form')
+                .send()
+                .end((err, res) => {
+                    expect(res.status).to.equal(200)
+                    expect(res.body.message).to.be.a('string')
+                    expect(res.body.message).to.equal('Email Verified')
                     done()
                 })
         })
     })
-    describe('/GET user', () => {
-        it('it should GET all users', (done) => {
+    describe('Login user', () => {
+        let refreshToken = '',
+            accessToken = ''
+        it('it should login a User', (done) => {
             chai.request(server)
-                .get('/user')
+                .post('/user/login')
+                .type('form')
+                .send({
+                    username: 'humaidk2',
+                    email: 'hum@google.com',
+                    password: 'password',
+                })
                 .end((err, res) => {
                     expect(res.status).to.equal(200)
-                    for (let i = 0; i < users.length; i++)
-                        expect(res.body[i].Id).to.equal(users[i].Id)
+                    expect(res.body.token).to.be.a('string')
+                    refreshToken = res.body.token
                     done()
                 })
         })
-        it('it should GET a specific user using user Id', (done) => {
+        it('it should have a valid refresh token', (done) => {
             chai.request(server)
-                .get('/user/user0')
+                .post('/user/refreshToken')
+                .type('form')
+                .send({
+                    token: refreshToken,
+                })
                 .end((err, res) => {
                     expect(res.status).to.equal(200)
-                    expect(res.body.Id).to.equal(users[0].Id)
+                    for (const key in users[0]) {
+                        expect(res.body.decoded[key]).to.equal(users[0][key])
+                    }
                     done()
                 })
         })
-        it('it should not GET a specific a missing user', (done) => {
+        it('it should trade refresh token for access token', (done) => {
             chai.request(server)
-                .get('/user/user1')
+                .post('/user/refresh')
+                .type('form')
+                .send({
+                    token: refreshToken,
+                })
                 .end((err, res) => {
-                    expect(res.status).to.equal(404)
-                    expect(res.body).to.deep.equal({
-                        message: 'User not found',
-                    })
+                    expect(res.status).to.equal(200)
+                    expect(res.body.token).to.be.a('string')
+                    accessToken = res.body.token
+                    done()
+                })
+        })
+        it('it should have a valid access token', (done) => {
+            chai.request(server)
+                .post('/user/accessToken')
+                .type('form')
+                .send({
+                    token: accessToken,
+                })
+                .end((err, res) => {
+                    expect(res.status).to.equal(200)
+                    for (const key in users[0]) {
+                        expect(res.body.decoded[key]).to.equal(users[0][key])
+                    }
                     done()
                 })
         })
