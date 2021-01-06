60b7d2cbf40bab666f74944e2202ea801f4fcbc4
Humaid Khan <humaidk2@gmail.com>
2021-01-06 18:25:06 +0400

    Add loading state Display loading page if state is loading Load Refresh token from keystore If no refresh token or invalid refresh token, load auth stack screens On valid refresh token, load diary entries screens

--- rn_journal/App.tsx
+++ rn_journal/App.tsx
@@ -9,7 +9,7 @@
  */
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     SafeAreaView,
     StyleSheet,
@@ -25,23 +25,130 @@ import {
 
 import Signup from './pages/Signup'
 import Login from './pages/Login'
+import Diary from './pages/Diary'
+import EntryForm from './pages/EntryForm'
+import Loading from './pages/Loading'
+
+const { KeyStoreModule } = NativeModules
 
 const Stack = createStackNavigator()
 const App = () => {
+    const [refresh, setRefresh] = useState({
+        token: '',
+        isLoading: true,
+    })
+    useEffect(() => {
+        let isMounted = true
+        ;(async () => {
+            await PermissionsAndroid.requestMultiple([
+                PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+                PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+            ])
+            try {
+                const refreshToken = await KeyStoreModule.getKey(
+                    'refreshToken',
+                    'journalPassword'
+                )
+                if (refreshToken === '') {
+                    if (isMounted) {
+                        return {
+                            token: '',
+                            isLoading: false,
+                        }
+                    }
+                }
+                const userJson = await fetch(
+                    'http://10.0.1.19:3000/user/refreshToken',
+                    {
+                        method: 'POST',
+                        headers: {
+                            Accept: 'application/json',
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            token: refreshToken,
+                        }),
+                    }
+                )
+                if (userJson.status !== 200) {
+                    return {
+                        token: '',
+                        isLoading: false,
+                    }
+                } else {
+                    return {
+                        token: refreshToken,
+                        isLoading: false,
+                    }
+                }
+            } catch (error) {
+                // if (isMounted)
+                //     setRefresh({
+                //         token: '',
+                //         isLoading: false,
+                //     })
+            }
+        })().then((data: any) => {
+            if (isMounted) setRefresh(data)
+            return null
+        })
+        return () => {
+            isMounted = false
+        }
+    }, [])
+    const signin = (token: string) => {
+        setRefresh({
+            token: token,
+            isLoading: false,
+        })
+    }
+    const logout = () => setRefresh({ token: '', isLoading: false })
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen
-                    name="Login"
-                    component={Login}
-                    options={{ title: 'Login', headerShown: false }}
-                />
-                <Stack.Screen
-                    name="Signup"
-                    component={Signup}
-                    options={{ title: 'Signup', headerShown: false }}
-                />
-            </Stack.Navigator>
+            {refresh.isLoading ? (
+                <Loading />
+            ) : refresh.token === '' ? (
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="Login"
+                        options={{ title: 'Login', headerShown: false }}
+                    >
+                        {(props) => <Login signin={signin} {...props} />}
+                    </Stack.Screen>
+                    <Stack.Screen
+                        name="Signup"
+                        component={Signup}
+                        options={{ title: 'Signup', headerShown: false }}
+                    />
+                </Stack.Navigator>
+            ) : (
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="Diary"
+                        options={{ title: 'Diary', headerShown: false }}
+                    >
+                        {(props) => (
+                            <Diary
+                                refreshToken={refresh.token}
+                                logout={logout}
+                                {...props}
+                            />
+                        )}
+                    </Stack.Screen>
+                    <Stack.Screen
+                        name="EntryForm"
+                        options={{ title: 'EntryForm', headerShown: false }}
+                    >
+                        {(props) => (
+                            <EntryForm
+                                refreshToken={refresh.token}
+                                logout={logout}
+                                {...props}
+                            />
+                        )}
+                    </Stack.Screen>
+                </Stack.Navigator>
+            )}
         </NavigationContainer>
     )
 }
